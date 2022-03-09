c02698e928a4e95f0134472fccde187b0ca45b56
Utku SENOCAK <u.senocakk@gmail.com>
2022-03-09 21:57:52 +0300

    UsersignupPage form layout created

--- frontend/src/UserSignupPage.js
+++ frontend/src/UserSignupPage.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
 
 class UserSignupPage extends React.Component {
-    render() {
-        return (
-            <h1>Sign Up</h1>
-        );
-    }
+  render() {
+    return (
+      <form>
+        <h1>Sign Up</h1>
+        <div>
+          <label>Username</label>
+          <input />
+        </div>
+        <div>
+          <label>Display Name</label>
+          <input />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" />
+        </div>
+        <div>
+          <label>Password Repeat</label>
+          <input type="password" />
+        </div>
+        <button>Sign Up</button>
+      </form>
+    );
+  }
 }
 
 export default UserSignupPage;
