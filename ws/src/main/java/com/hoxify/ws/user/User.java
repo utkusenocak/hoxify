26e10254dc409db1126e86b5afc9a150b9573374
Utku SENOCAK <u.senocakk@gmail.com>
2022-03-12 17:12:56 +0300

    Saving User to Database

--- ws/src/main/java/com/hoxify/ws/user/User.java
+++ ws/src/main/java/com/hoxify/ws/user/User.java
@@ -2,8 +2,18 @@ package com.hoxify.ws.user;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
     private String username;
 
     private String displayName;
--- ws/src/main/java/com/hoxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoxify/ws/user/UserController.java
@@ -1,8 +1,6 @@
 package com.hoxify.ws.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    @Autowired
+    UserRepository userRepository;
 
     @PostMapping("/api/1.0/users")
     public void createUser(@RequestBody User user) {
-        log.info(user.toString());
+        userRepository.save(user);
     }
 }
