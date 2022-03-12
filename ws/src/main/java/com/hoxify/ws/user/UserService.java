c11b4f086d0fb167007eb597e34d00b98257c24c
Utku SENOCAK <u.senocakk@gmail.com>
2022-03-12 17:37:04 +0300

    Storing Password in Database

--- ws/src/main/java/com/hoxify/ws/user/UserService.java
+++ ws/src/main/java/com/hoxify/ws/user/UserService.java
@@ -1,5 +1,7 @@
 package com.hoxify.ws.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,11 +10,15 @@ public class UserService {
 
     UserRepository userRepository;
 
+    PasswordEncoder passwordEncoder;
+
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public void save(User user) {
+        user.setPassword(this.passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 }
