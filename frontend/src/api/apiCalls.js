d5807f7c9fa8bed741a1905a717821f9e5fa0654
Utku SENOCAK <u.senocakk@gmail.com>
2022-03-17 21:39:13 +0300

    Change Language added React

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+axios.defaults.headers["accept-language"] = "en";
+
 export const signup = (body) => {
-  return axios.post("/api/1.0/users", body, {headers : {"accept-language": "en"}});
+  return axios.post("/api/1.0/users", body);
+};
+
+export const changeLanguage = (language) => {
+  axios.defaults.headers["accept-language"] = language;
 };
--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { signup } from "../api/apiCalls";
+import { signup, changeLanguage } from "../api/apiCalls";
 import Input from "../components/Input";
 import { withTranslation } from "react-i18next";
 
@@ -51,6 +51,12 @@ class UserSignupPage extends React.Component {
     this.setState({ pendingApiCall: false });
   };
 
+  onChangeLanguage = (language) => {
+    const { i18n } = this.props;
+    i18n.changeLanguage(language);
+    changeLanguage(language);
+  };
+
   render() {
     const { pendingApiCall, errors } = this.state;
     const { username, displayName, password, passwordRepeat } = errors;
@@ -96,6 +102,24 @@ class UserSignupPage extends React.Component {
               )}
               {t("Sign Up")}
             </button>
+          </div>
+          <div>
+            <img
+              src="https://flagcdn.com/h24/tr.png"
+              srcset="https://flagcdn.com/h48/tr.png 2x"
+              height="24"
+              alt="Turkey"
+              onClick={() => this.onChangeLanguage("tr")}
+              style={{ cursor: "pointer" }}
+            />
+            <img
+              src="https://flagcdn.com/h24/us.png"
+              srcset="https://flagcdn.com/h48/us.png 2x"
+              height="24"
+              alt="United States"
+              onClick={() => this.onChangeLanguage("en")}
+              style={{ cursor: "pointer" }}
+            />
           </div>
         </form>
       </div>
