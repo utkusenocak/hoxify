9b13f2a22eacb7c6b5c5532b0d09e389cc56a7c5
Utku SENOCAK <u.senocakk@gmail.com>
2022-03-22 22:19:59 +0300

    Authentication controller

---
+++ ws/src/main/java/com/hoxify/ws/auth/AuthController.java
@@ -0,0 +1,51 @@
+package com.hoxify.ws.auth;
+
+import com.hoxify.ws.error.ApiError;
+import com.hoxify.ws.user.User;
+import com.hoxify.ws.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Base64;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @PostMapping("/api/1.0/auth")
+    @ResponseStatus(HttpStatus.CREATED)
+    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization", required = false) String authorization) {
+        if (authorization == null) {
+            ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+        }
+        String base64Encoded = authorization.split("Basic ")[1];
+        String decoded = new String(Base64.getDecoder().decode(base64Encoded));
+        String[] parts = decoded.split(":");
+        String username = parts[0];
+        String password = parts[1];
+        User inDb = userRepository.findByUsername(username);
+        if (inDb == null) {
+            ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+        }
+        String hashedPassword = inDb.getPassword();
+        if (!passwordEncoder.matches(password, hashedPassword)) {
+            ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+        }
+        return ResponseEntity.ok().build();
+
+    }
+}
