b3c72e39000eaf7ccb9a8bf6d7a62a879c468796
Utku SENOCAK <u.senocakk@gmail.com>
2022-03-22 21:17:30 +0300

    Authentication added react

--- frontend/src/pages/UserLoginPage.js
+++ frontend/src/pages/UserLoginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Input from "../components/Input";
 import { withTranslation } from "react-i18next";
+import { login } from "../api/apiCalls";
 
 class UserLoginPage extends React.Component {
   state = {
@@ -14,6 +15,17 @@ class UserLoginPage extends React.Component {
       [name]: value,
     });
   };
+
+  onClickLogin = (event) => {
+    event.preventDefault();
+    const { username, password } = this.state;
+    const creds = {
+      username,
+      password,
+    };
+    login(creds);
+  };
+
   render() {
     const { t } = this.props;
     return (
@@ -32,7 +44,9 @@ class UserLoginPage extends React.Component {
             onChange={this.onChange}
           />
           <div className="text-center">
-            <button className="btn btn-primary">{t("Login")}</button>
+            <button className="btn btn-primary" onClick={this.onClickLogin}>
+              {t("Login")}
+            </button>
           </div>
         </form>
       </div>
