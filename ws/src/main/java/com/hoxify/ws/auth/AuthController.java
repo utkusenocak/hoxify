0f3338607f44cc427310a271cbbcc4d29447bebf
Utku SENOCAK <u.senocakk@gmail.com>
2022-03-25 16:03:07 +0300

    Error handler for all error cases

--- ws/src/main/java/com/hoxify/ws/auth/AuthController.java
+++ ws/src/main/java/com/hoxify/ws/auth/AuthController.java
@@ -1,15 +1,11 @@
 package com.hoxify.ws.auth;
 
 import com.fasterxml.jackson.annotation.JsonView;
-import com.hoxify.ws.error.ApiError;
 import com.hoxify.ws.shared.Views;
 import com.hoxify.ws.user.User;
 import com.hoxify.ws.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,30 +18,16 @@ public class AuthController {
     @Autowired
     UserRepository userRepository;
 
-    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
     @PostMapping("/api/1.0/auth")
     @JsonView(Views.Base.class)
-    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization", required = false) String authorization) {
-        if (authorization == null) {
-            ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-        }
+    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization") String authorization) {
+
         String base64Encoded = authorization.split("Basic ")[1];
         String decoded = new String(Base64.getDecoder().decode(base64Encoded));
         String[] parts = decoded.split(":");
         String username = parts[0];
-        String password = parts[1];
         User inDb = userRepository.findByUsername(username);
-        if (inDb == null) {
-            ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-        }
-        String hashedPassword = inDb.getPassword();
-        if (!passwordEncoder.matches(password, hashedPassword)) {
-            ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-        }
+
         return ResponseEntity.ok(inDb);
 
     }
--- ws/src/main/java/com/hoxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoxify/ws/user/UserController.java
@@ -1,16 +1,11 @@
 package com.hoxify.ws.user;
 
-import com.hoxify.ws.error.ApiError;
 import com.hoxify.ws.shared.GenericResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class UserController {
@@ -23,17 +18,5 @@ public class UserController {
     public GenericResponse createUser(@Valid @RequestBody User user) {
         userService.save(user);
         return new GenericResponse("user created");
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleValidationException(MethodArgumentNotValidException exception) {
-        ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
-        Map<String, String> validationErrors = new HashMap<>();
-        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-        }
-        error.setValidationErrors(validationErrors);
-        return error;
     }
 }
