d4d903116545e48c06912fb7eb83abe23b65999d
Utku SENOCAK <u.senocakk@gmail.com>
2022-04-26 19:31:48 +0300

    Persisting state

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../redux/authSlice";
+const hoaxAuth = localStorage.getItem("hoax-auth");
+let stateInLocalStorage = {
+  isLoggedIn: false,
+  username: undefined,
+  displayName: undefined,
+  image: undefined,
+  password: undefined,
+};
+if (hoaxAuth) {
+  try {
+    stateInLocalStorage = JSON.parse(hoaxAuth);
+  } catch (error) {}
+}
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+  preloadedState: stateInLocalStorage,
 });
+store.subscribe(() => {
+  localStorage.setItem("hoax-auth", JSON.stringify(store.getState()));
+});
+export default store;
