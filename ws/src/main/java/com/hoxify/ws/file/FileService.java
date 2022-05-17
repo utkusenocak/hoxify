bcbafa8087c8bc4873f6b8a6151bbf403e3b438d
Utku SENOCAK <u.senocakk@gmail.com>
2022-05-17 12:11:33 +0300

    Checking file type

--- ws/src/main/java/com/hoxify/ws/file/FileService.java
+++ ws/src/main/java/com/hoxify/ws/file/FileService.java
@@ -1,15 +1,16 @@
 package com.hoxify.ws.file;
 
 import com.hoxify.ws.configuration.AppConfiguration;
+import org.apache.tika.Tika;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.UUID;
@@ -20,11 +21,16 @@ public class FileService {
     @Autowired
     AppConfiguration appConfiguration;
 
+    private static final Logger log = LoggerFactory.getLogger(FileService.class);
+
     public String writeBase64EncodedStringToFile(String image) throws IOException {
+        Tika tika = new Tika();
         String filename = generateRandomName();
         File target = new File(appConfiguration.getUploadPath() +"/"+filename);
         OutputStream outputStream = Files.newOutputStream(target.toPath());
         byte[] base64encoded = Base64.getDecoder().decode(image);
+        String fileType = tika.detect(base64encoded);
+        log.info(fileType);
         outputStream.write(base64encoded);
         outputStream.close();
         return filename;
