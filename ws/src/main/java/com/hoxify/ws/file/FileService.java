7d34f750cccba054f17f721b0df87d70706b3fb5
Utku SENOCAK <u.senocakk@gmail.com>
2022-05-17 11:30:11 +0300

    Configuration and profiles

--- ws/src/main/java/com/hoxify/ws/file/FileService.java
+++ ws/src/main/java/com/hoxify/ws/file/FileService.java
@@ -1,5 +1,8 @@
 package com.hoxify.ws.file;
 
+import com.hoxify.ws.configuration.AppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -11,9 +14,13 @@ import java.util.UUID;
 
 @Service
 public class FileService {
+
+    @Autowired
+    AppConfiguration appConfiguration;
+
     public String writeBase64EncodedStringToFile(String image) throws IOException {
         String filename = generateRandomName();
-        File target = new File("pictures/"+filename);
+        File target = new File(appConfiguration.getUploadPath() +"/"+filename);
         OutputStream outputStream = Files.newOutputStream(target.toPath());
         byte[] base64encoded = Base64.getDecoder().decode(image);
         outputStream.write(base64encoded);
